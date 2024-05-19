50eb226d2e796f7642042a58b64fd962320e55d9
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2024-05-19 12:37:32 +0600

    db connected

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('heelo')
+const express=require('express')
+const app=express()
+const mongoose=require('mongoose')
+const dotenv=require('dotenv').config()
+const userRoute=require('./routes/user')
+const connectDB=require('./db/db')
+const port=process.env.PORT||5000
+
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.send('success')
+})
+app.use('/api',userRoute)
+
+const start=()=>{
+    try{
+        connectDB(process.env.MONGO_URI);
+        app.listen(port,()=>{
+            console.log(`server started at port: ${port}`)
+        })
+    }catch(error){
+        console.log("error")
+    }
+}
+start()
